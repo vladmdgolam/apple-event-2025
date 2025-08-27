8d8dc674092acdeadaff620626f4ee94e198fdb0
Vlad <vladmdgolam@gmail.com>
2025-08-27 02:39:45 +0200

    Refactor Home component to use direct import for AppleHeatmapScene, removing dynamic import and SSR handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import dynamic from 'next/dynamic'
-
-// Dynamically import to avoid SSR issues with Three.js
-const AppleHeatmapScene = dynamic(() => import('../components/AppleHeatmapScene').then(mod => ({ default: mod.AppleHeatmapScene })), {
-  ssr: false,
-  loading: () => <div className="w-full h-screen bg-black flex items-center justify-center text-white">Loading...</div>
-})
+import { AppleHeatmapScene } from "@/components/AppleHeatmapScene"
 
 export default function Home() {
   return (
     <div className="w-full h-screen bg-black">
       <AppleHeatmapScene />
     </div>
-  );
+  )
 }
